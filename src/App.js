f6038fb1fb9fca7a9dbc11290fdff7781713f65e
saifilwebisoft <saifilmomin4@gmail.com>
2022-11-29 18:57:34 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+function App() {
+
+  const a =(`"{",A1,":",A2,B1,":",A2"}"`)
+
+  return (
+    <div>
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/starburst/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const StarBurst = () => {
+
+
+  return <div>StarBurst</div>;
+};
+
+export default StarBurst;
